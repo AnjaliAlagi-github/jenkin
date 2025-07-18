2b3db1eb70c6fc1ae8f463b652d9fd3d3f9a8cfd
AnjaliAlagi-github <anjalialagi292001@gmail.com>
2025-07-18 23:46:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var http =require('http');
+
+http.createServer(function (req, res){
+  res.write('Welcome to AWS with Anjali');
+  res.end();
+}).listen(3000, '0.0.0.0',() =>{
+  console.log('server running on port 3000');
+});
